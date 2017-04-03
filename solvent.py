bdc7a36e119b72b00aae375fce8c2ad2cb4dfa52
Yuto Mizutani <i.yuto.curiosity@gmail.com>
2017-04-03 14:24:45 +0900

    first commit

---
+++ solvent.py
@@ -0,0 +1,38 @@
+'''
+溶媒のパラメータを指定するファイル.
+辞書型のkeyに対して適切な単位の値を指定すること.
+sigma[Å]
+epsilon[kcal/mol]
+'''
+
+solvent = {'name':"Ar", 'sigma': 3.4, 'epsilon': 6.63}
+
+def LJ_potential(x):
+    '''np.array型のxを受け取るとそれに対応するLJポテンシャルの値を返す
+    グラフを書くときにarray型のyの値が必要になるためである.
+    sigma_rはLJポテンシャル内のsigma/rを先に計算してしまう変数であり1e-15を分子に足しているのは0割りを防ぐためである.
+    '''
+    sigma_r = solvent['sigma']/(x + 1e-15)
+    return 4*solvent['epsilon']*(sigma_r ** 12 - sigma_r ** 6)
+
+def plot_LJpotential():
+    '''
+    LJポテンシャルをプロットするプログラム
+    x,yの範囲はsigma,epsilonの倍にとってある
+    '''
+    import matplotlib.pyplot as plt
+    import numpy as np
+    x = np.arange(0, 2 * solvent['sigma'], 0.1)
+    print(x)
+    y = LJ_potential(x)
+    print(y)
+    plt.plot(x,y)
+    plt.xlim(0,2 * solvent['sigma'])
+    plt.ylim(-2 * solvent['epsilon'], 2 * solvent['epsilon'])
+    plt.show()
+
+if __name__ == '__main__':
+    '''
+    このプログラムを単体で動かすとsolventのLJポテンシャルの様子がプロットされる
+    '''
+    plot_LJpotential()
